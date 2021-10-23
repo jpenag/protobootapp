1d5855c419371218a9d8451ce8ce0622729faa18
jpenag <jpenag@eafit.edu.co>
2021-10-22 20:43:59 -0500

    Cambios en calculadora

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -9,4 +9,9 @@ public class Calculadora {
     public Double cubo(Double numero) {
         return numero * numero * numero;
     }
+    
+    public Double aCuatro(Double numero){
+        return numero * numero * numero * numero;
+    }
+    
 }
